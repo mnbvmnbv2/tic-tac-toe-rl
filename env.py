f97c56461581f30371c58f81fec2d270a7d715e3
mnbvmnbv2 <ferdinand@ytteborg.com>
2024-10-08 20:52:34 +0200

    batched pure python

--- env.py
+++ env.py
@@ -3,11 +3,10 @@ import time
 import numpy as np
 
 import gymnasium
-import pufferlib
 
 
-class TicTacToeEnvSingle(pufferlib.PufferEnv):
-    def __init__(self) -> None:
+class TicTacToeEnvSingle:
+    def __init__(self, batch_size: int = 1) -> None:
         self.observation_space = gymnasium.spaces.Box(
             low=0, high=1, shape=(18,), dtype=np.uint8
         )
@@ -17,53 +16,66 @@ class TicTacToeEnvSingle(pufferlib.PufferEnv):
         self.num_agents = 1
         self.render_mode = None
         self.emulated = None
-        self.done = False
-        self.buf = pufferlib.namespace(
-            observations=np.zeros((1, 18), dtype=np.uint8),
-            rewards=np.zeros(1, dtype=np.float32),
-            terminals=np.zeros(1, dtype=bool),
-            truncations=np.zeros(1, dtype=bool),
-            masks=np.ones(1, dtype=bool),
-        )
-        self.actions = np.zeros(1, dtype=np.uint32)
 
-        self.game_state = np.zeros(9, dtype=np.uint8)
+        self._batch_size = batch_size
 
-    def get_obs(self) -> np.ndarray:
+        self.game_states = np.zeros((batch_size, 9), dtype=np.uint8)
+        self.reward = np.zeros(self._batch_size, dtype=np.float32)
+        self.terminated = np.zeros(self._batch_size, dtype=bool)
+        self.truncated = np.zeros(self._batch_size, dtype=bool)
+        self.info = [{} for _ in range(self._batch_size)]
+        self.observation = np.zeros((self._batch_size, 18), dtype=np.uint8)
+
+        self._winners = np.zeros(batch_size, dtype=np.uint8)
+
+    def calc_obs(self) -> None:
         # flatten one-hot encoding
-        return np.eye(3)[self.game_state][:, 1:].flatten()
+        self.observation = (
+            np.eye(3)[self.game_state][:, :, 1:].flatten().reshape(self._batch_size, 18)
+        )
 
-    def check_win(self) -> int:
+    def check_win(self) -> None:
         # 0 for tie, 1 for player 1, 2 for player 2
         # rows
-        for i in range(3):
+        for g in range(self._batch_size):
+            for i in range(3):
+                if (
+                    self.game_state[g, i * 3]
+                    == self.game_state[g, i * 3 + 1]
+                    == self.game_state[g, i * 3 + 2]
+                    != 0
+                ):
+                    self._winners[g] = self.game_state[g, i * 3]
+            # columns
+            for i in range(3):
+                if (
+                    self.game_state[g, i]
+                    == self.game_state[g, i + 3]
+                    == self.game_state[g, i + 6]
+                    != 0
+                ):
+                    self._winners[g] = self.game_state[g, i]
+            # diagonals
             if (
-                self.game_state[i * 3]
-                == self.game_state[i * 3 + 1]
-                == self.game_state[i * 3 + 2]
+                self.game_state[g, 0]
+                == self.game_state[g, 4]
+                == self.game_state[g, 8]
                 != 0
             ):
-                return self.game_state[i * 3]
-        # columns
-        for i in range(3):
+                self._winners[g] = self.game_state[g, 0]
             if (
-                self.game_state[i]
-                == self.game_state[i + 3]
-                == self.game_state[i + 6]
+                self.game_state[g, 2]
+                == self.game_state[g, 4]
+                == self.game_state[g, 6]
                 != 0
             ):
-                return self.game_state[i]
-        # diagonals
-        if self.game_state[0] == self.game_state[4] == self.game_state[8] != 0:
-            return self.game_state[0]
-        if self.game_state[2] == self.game_state[4] == self.game_state[6] != 0:
-            return self.game_state[2]
-        return 0
+                self._winners[g] = self.game_state[g, 2]
+            self._winners[g] = 0
 
     def reset(self, seed=None) -> tuple[np.ndarray, dict]:
         # obs, info
-        self.game_state = np.zeros(9, dtype=np.uint8)
-        return self.get_obs(), {}
+        self.game_state = np.zeros((self._batch_size, 9), dtype=np.uint8)
+        return self.calc_obs(), {}
 
     def nice_print(self) -> str:
         return "\n".join(
@@ -75,37 +87,51 @@ class TicTacToeEnvSingle(pufferlib.PufferEnv):
             ]
         )
 
-    def step(self, action: int) -> tuple[np.ndarray, float, bool, bool, dict]:
+    def step(self, action: np.ndarray) -> tuple[np.ndarray, float, bool, bool, dict]:
         # obs, reward, terminated, truncated, info
-        # if illegal move
-        if self.game_state[action] > 0:
-            return self.get_obs(), -1, False, False, {}
-        # else, player performs the action
-        self.game_state[action] = 1
+        for g in range(self._batch_size):
+            # if illegal move
+            if self.game_state[g, action[g]] > 0:
+                self.reward[g] = -1
+                self.terminated[g] = False
+                self.truncated[g] = False
+                continue
+            # else, player performs the action
+            self.game_state[g, action[g]] = 1
 
-        # check if done (player 1 is always last in tied game)
-        is_done = np.all(self.game_state > 0)
-        winner = self.check_win()
-        if winner > 0 or is_done:
-            match winner:
-                case 0:
-                    reward = 0
-                case 1:
-                    reward = 1
-                case 2:
-                    reward = -1
-            return self.get_obs(), reward, True, False, {}
+            # check if done (player 1 is always last in tied game)
+            is_done = np.all(self.game_state[g] > 0)
+            self.check_win()
+            if self._winners[g] > 0 or is_done:
+                match self._winners[g]:
+                    case 0:
+                        reward = 0
+                    case 1:
+                        reward = 1
+                    case 2:
+                        reward = -1
+                self.reward[g] = reward
+                self.terminated[g] = True
+                self.truncated[g] = False
+                continue
 
-        # random move by the opponent
-        opponent_action = np.where(self.game_state == 0)[0]
-        opponent_action = np.random.choice(opponent_action)
-        self.game_state[opponent_action] = 2
-        winner = self.check_win()
-        if winner > 0:
-            # only player 2 can win here
-            return self.get_obs(), -1, True, False, {}
-        # else we continue the game
-        return self.get_obs(), 0, False, False, {}
+            # random move by the opponent
+            opponent_action = np.where(self.game_state[g] == 0)[0]
+            opponent_action = np.random.choice(opponent_action)
+            self.game_state[g, opponent_action] = 2
+            self.check_win()
+            if self._winners > 0:
+                # only player 2 can win here
+                self.reward[g] = -1
+                self.terminated[g] = True
+                self.truncated[g] = False
+                continue
+            # else we continue the game
+            self.reward[g] = 0
+            self.terminated[g] = False
+            self.truncated[g] = False
+        self.calc_obs()
+        return self.observation, self.reward, self.terminated, self.truncated, self.info
 
 
 def test():
@@ -125,18 +151,20 @@ def test():
             break
 
 
-def speed_test():
-    env = TicTacToeEnvSingle()
+def speed_test(dims=1):
+    env = TicTacToeEnvSingle(batch_size=dims)
     pre_time = time.time()
     num_steps = 0
-    while time.time() - pre_time < 5:
+    rng = np.random.default_rng()
+    while time.time() - pre_time < 1:
         obs, info = env.reset()
         terminated = False
         while not terminated:
-            obs, reward, terminated, truncated, info = env.step(np.random.randint(9))
+            action = rng.integers(9, size=env._batch_size)
+            obs, reward, terminated, truncated, info = env.step(action)
             num_steps += 1
-    print(f"Ran {num_steps} steps in 5 second")
+    print(f"Ran {num_steps} steps in 1 second")
 
 
 if __name__ == "__main__":
-    speed_test()
+    speed_test(1)
