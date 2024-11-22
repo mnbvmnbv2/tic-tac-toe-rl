2a490ce180919d862c52814b489dab5745ed9afc
mnbvmnbv2 <ferdinand@ytteborg.com>
2024-11-22 21:53:14 +0100

    fix jax code

--- envs/env_jax.py
+++ envs/env_jax.py
@@ -7,8 +7,6 @@ import jax.numpy as jnp
 from jax import lax
 from gymnax.environments import environment, spaces
 
-jax.config.update("jax_enable_x64", True)
-
 
 @struct.dataclass
 class EnvState(environment.EnvState):
@@ -30,7 +28,6 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
     def __init__(self):
         super().__init__()
         self.obs_shape = (18,)
-        self._step = -1
 
     @property
     def default_params(self) -> EnvParams:
@@ -42,7 +39,7 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
             [state.board == 1, state.board == 2], axis=0, dtype=jnp.int32
         )
 
-    def check_win(self, board: jnp.ndarray) -> int:
+    def check_win(self, state: EnvState) -> int:
         # Win conditions defined as indices on the board
         win_conditions = jnp.array(
             [
@@ -60,10 +57,10 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
 
         def check_line(winner, line):
             line_win = lax.cond(
-                (board[line[0]] == board[line[1]])
-                & (board[line[1]] == board[line[2]])
-                & (board[line[0]] != 0),
-                lambda: board[line[0]].astype(jnp.int32),  # Ensuring int32 output
+                (state.board[line[0]] == state.board[line[1]])
+                & (state.board[line[1]] == state.board[line[2]])
+                & (state.board[line[0]] != 0),
+                lambda: state.board[line[0]].astype(jnp.int32),  # Ensuring int32 output
                 lambda: jnp.array(0, dtype=jnp.int32),  # Ensuring int32 output
             )
             return jnp.maximum(winner, line_win), None
@@ -76,13 +73,15 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
         return self.get_obs(state), state, jnp.array(params.rew_illegal), True, {}
 
     def step_env(
-        self, key: jax.random.PRNGKey, state: EnvState, action: int, params: EnvParams
+        self,
+        key: jax.random.PRNGKey,
+        state: EnvState,
+        action: jnp.ndarray,
+        params: EnvParams,
     ) -> step_return:
-        self._step += 1
         # Check for illegal move
-        illegal_move = state.board[action] != 0
         return lax.cond(
-            illegal_move,
+            state.board[action[0]] != 0,
             lambda: self.handle_illegal_move(params, state),
             lambda: self.perform_player_move(key, state, action, params),
         )
@@ -92,8 +91,8 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
     ) -> step_return:
         # Update board with player move
         new_board = state.board.at[action].set(1)
-        new_state = EnvState(self._step, new_board)
-        winner = self.check_win(new_board)
+        new_state = EnvState(state.time, new_board)
+        winner = self.check_win(new_state)
 
         # Check if player won or if it's a tie
         player_wins = winner == 1
@@ -150,8 +149,8 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
             lambda _: state.board,
             operand=opponent_action,
         )
-        new_state = EnvState(self._step, new_board)
-        winner = self.check_win(new_board)
+        new_state = EnvState(state.time, new_board)
+        winner = self.check_win(new_state)
 
         # Check if opponent won or if it's a tie
         opponent_wins = winner == 2
@@ -184,12 +183,11 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
         key: chex.PRNGKey,
         params: EnvParams,
     ) -> tuple[chex.Array, EnvState]:
-        self._step = -1
         state = EnvState(time=0, board=jnp.zeros((9), dtype=jnp.int32))
         return self.get_obs(state), state
 
     def is_terminal(self, state: EnvState, params: EnvParams) -> jnp.ndarray:
-        winner = check_win(state)
+        winner = self.check_win(state)
         return jnp.logical_or(winner > 0, jnp.all(state.board > 0, axis=1))
 
     @property
@@ -210,140 +208,32 @@ class TicTacToeEnv(environment.Environment[EnvState, EnvParams]):
         return spaces.Box(low=0, high=2, shape=(9,))
 
 
-@jax.jit
-def check_win(state) -> int:
-    reshaped_state = state.board.reshape(3, 3)
-    # 0 for tie, 1 for player 1, 2 for player 2
-    # Check for row-wise winners
-    rows_equal = jnp.all(reshaped_state == reshaped_state[:, [0]], axis=1) & (
-        reshaped_state[:, 0] != 0
-    )
-    row_winners = reshaped_state[:, 0] * rows_equal
-
-    # Check for column-wise winners
-    cols_equal = jnp.all(reshaped_state == reshaped_state[[0], :], axis=0) & (
-        reshaped_state[0, :] != 0
-    )
-    col_winners = reshaped_state[0, :] * cols_equal
-
-    # Check for diagonal winners
-    diagonal_1 = (
-        (reshaped_state[0, 0] != 0)
-        & (reshaped_state[0, 0] == reshaped_state[1, 1])
-        & (reshaped_state[0, 0] == reshaped_state[2, 2])
-    )
-    diagonal_1_winners = reshaped_state[0, 0] * diagonal_1
-
-    diagonal_2 = (
-        (reshaped_state[0, 2] != 0)
-        & (reshaped_state[0, 2] == reshaped_state[1, 1])
-        & (reshaped_state[0, 2] == reshaped_state[2, 0])
-    )
-    diagonal_2_winners = reshaped_state[0, 2] * diagonal_2
-
-    # Return the maximum winner (1 for player 1, 2 for player 2, 0 for no winner)
-    return jnp.maximum(
-        row_winners.max(),
-        jnp.maximum(
-            col_winners.max(),
-            jnp.maximum(diagonal_1_winners, diagonal_2_winners),
-        ),
-    )
-
-
 # Define function to perform step speed test
-def step_speed_test(rng, step_fn, env, state, env_params, duration=1.0):
+def step_speed_test(rng, env, state, env_params, duration=1.0):
+    rng, reset_rng, step_rng, action_rng = jax.random.split(rng, 4)
+    env.reset_env(reset_rng, env_params)
     # Warm-up to compile the function
-    rng, step_rng, action_rng = jax.random.split(rng, 3)
-    action = env.action_space(env_params).sample(action_rng)
-    _ = step_fn(step_rng, state, action, env_params)
+    pre_compile = time.time()
+    jit_step = jax.jit(env.step_env)
+    action = jax.random.randint(action_rng, 1, 0, env.num_actions)
+    _ = jit_step(step_rng, state, action, env_params)
+    env.reset_env(reset_rng, env_params)
+    print(f"Compilation time: {time.time() - pre_compile:.4f}s")
 
     start_time = time.time()
     steps = 0
     while time.time() - start_time < duration:
         rng, step_rng, action_rng = jax.random.split(rng, 3)
-        state = step_fn(step_rng, state, action, env_params)[1]
+        state = jit_step(step_rng, state, action, env_params)[1]
         steps += 1
-    return steps
-
-
-def test():
-    rng = jax.random.PRNGKey(0)
-    rng, key_reset = jax.random.split(rng)
-
-    # Instantiate the environment and its settings
-    env, env_params = TicTacToeEnv(), EnvParams()
-
-    # Reset the environment
-    obs, state = env.reset(key_reset, env_params)
-
-    # Perform speed test
-    num_steps_regular = step_speed_test(rng, env.step, env, state, env_params)
-    print(f"Number of regular steps in 1 second: {num_steps_regular}")
-
-
-# Define function to perform a batched step speed test
-def batched_step_speed_test(rng, step_fn, env, states, env_params, duration=1.0):
-    # Number of parallel environments
-    num_envs = states.board.shape[0]
-
-    # Warm-up to compile the function with a batch of actions and keys
-    rng, action_rng = jax.random.split(rng)
-    action_keys = jax.random.split(action_rng, num_envs)
-    actions = jax.vmap(env.action_space(env_params).sample)(action_keys)
-    rng, step_rng = jax.random.split(rng)
-    step_keys = jax.random.split(
-        step_rng, num_envs
-    )  # Generate unique keys for each environment
-    _ = step_fn(step_keys, states, actions, env_params)
-
-    start_time = time.time()
-    steps = 0
-
-    # Loop until the time duration is reached
-    while time.time() - start_time < duration:
-        # Split RNG to generate a new batch of action keys
-        rng, action_rng = jax.random.split(rng)
-        action_keys = jax.random.split(action_rng, num_envs)
-        actions = jax.vmap(env.action_space(env_params).sample)(action_keys)
-
-        # Split RNG to generate a new batch of step keys for each environment
-        rng, step_rng = jax.random.split(rng)
-        step_keys = jax.random.split(step_rng, num_envs)
-
-        # Update all environments in parallel using vmap and batched PRNG keys
-        states = step_fn(step_keys, states, actions, env_params)[1]
-        steps += 1
-
-    # Multiply by batch size to get total steps across all environments
-    return steps * num_envs
-
-
-def batched_test(num_envs=16000, duration=1.0):
-    rng = jax.random.PRNGKey(0)
-    rng, key_reset = jax.random.split(rng)
-
-    # Instantiate the environment and its parameters
-    env, env_params = TicTacToeEnv(), EnvParams()
-
-    # Initialize batched states for the specified number of environments
-    obs, states = jax.vmap(env.reset, in_axes=(0, None))(
-        jax.random.split(key_reset, num_envs), env_params
-    )
-
-    # Vectorize the environment step function for batched processing
-    batched_step = jax.vmap(env.step, in_axes=(0, 0, 0, None))
-
-    # Perform batched speed test
-    num_steps_batched = batched_step_speed_test(
-        rng, batched_step, env, states, env_params, duration
-    )
-    print(
-        f"Number of batched steps across {num_envs} environments in {duration} second(s): {num_steps_batched}"
-    )
+    print(f"Ran {steps} steps in {duration:.4f}s")
 
 
 if __name__ == "__main__":
     print(jax.devices())
     # test()
-    batched_test()
+    env, env_params = TicTacToeEnv(), EnvParams()
+    state = EnvState(time=0, board=jnp.zeros((9), dtype=jnp.int32))
+
+    rng = jax.random.PRNGKey(0)
+    step_speed_test(rng, env, state, env_params)
