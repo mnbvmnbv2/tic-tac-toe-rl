516fe869ce4028f19143f5dbb75efc40231ca252
mnbvmnbv2 <ferdinand@ytteborg.com>
2025-01-20 20:37:26 +0100

    setup with pvp

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-# Define the Cython extension for env.pyx
 extensions = [
     Extension(
         name="c_tictactoe",  # This should match the name of the .pyx file without the extension
         sources=["envs/c_tictactoe.pyx"],  # The Cython file to be compiled
         include_dirs=[np.get_include()],  # Include directory for numpy
-    )
+    ),
+    Extension(
+        name="c_tictactoe_pvp",
+        sources=["pvp/c_tictactoe_pvp.pyx"],
+        include_dirs=[np.get_include()],
+    ),
 ]
 
 setup(
-    name="C_TicTacToe",  # Name of your package
+    name="C_TicTacToe",
     ext_modules=cythonize(extensions, compiler_directives={"language_level": "3"}),
-    py_modules=["c_tictactoe"],  # Explicitly specify which modules to include
+    py_modules=["c_tictactoe", "c_tictactoe_pvp"],
 )
