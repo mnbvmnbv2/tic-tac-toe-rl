ddc08607e8d804415a6a4774b0ceeba763b82633
mnbvmnbv2 <ferdinand@ytteborg.com>
2025-02-06 21:19:25 +0100

    add env lib in rust

---
+++ rust/env/src/lib.rs
@@ -0,0 +1,17 @@
+/// Tic Tac Toe Environment module.
+pub mod tictactoe {
+    #[derive(Debug, Clone)]
+    pub struct TicTacToeEnv {
+        //
+    }
+
+    impl TicTacToeEnv {
+        pub fn new() -> Self {
+            TicTacToeEnv {}
+        }
+
+        pub fn reset(&mut self) {
+            //
+        }
+    }
+}
