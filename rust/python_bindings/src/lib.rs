85fe30aa9dd95e3d96d1b13dbf6492a0db461a4d
mnbvmnbv2 <ferdinand@ytteborg.com>
2025-10-20 21:16:11 +0200

    update cargo lock, remove greet in lib rs

--- rust/python_bindings/src/lib.rs
+++ rust/python_bindings/src/lib.rs
@@ -1,11 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-#[pyfunction]
-fn greet(name: &str) -> PyResult<String> {
-    Ok(format!("Hello, {}!", name))
-}
-
 #[pymodule]
 fn python_bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Use add_wrapped instead of add_function:
